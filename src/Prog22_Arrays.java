ad5c7f7dfb091baadd2f85b23301fbc333fe639b
Omkar <omkarsalunke1978@gmail.com>
2024-12-10 19:06:09 +0530

    rename class

--- src/Prog22_Arrays.java
+++ src/Prog22_Arrays.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pro22_Arrays {
+public class Prog22_Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // syntax:
